ed91c8affd3e99b1836b6c83be4a7cb7ec212cd0
Michael Dorman <michaelw.kcs@gmail.com>
2025-04-21 21:16:55 -0400

    add generations migration

--- db/migrations/env.py
+++ db/migrations/env.py
@@ -3,7 +3,7 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from alembic import context
-from models.drawing import Base
+from models.generations import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
